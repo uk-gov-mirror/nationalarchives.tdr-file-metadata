01b02f73daad52bc2b41622fb7fd5244cc4cbf2c
Sam Palmer <github@sampalmer.dev>
2020-03-12 16:38:30 +0000

    Add tests for the remaining file info.

--- test/index.test.ts
+++ test/index.test.ts
@@ -13,8 +13,31 @@ test("returns the correct checksum for a file", async () => {
 test("returns correct number of results", async () => {
   const blob = new Blob([new Uint8Array(fs.readFileSync("test/testfile"))])
   const file: File = new File([blob], "")
-  const { lastModified, size } = file
 
   const result = await getFileInformation([file, file, file])
   expect(result).toHaveLength(3)
+})
+
+test("returns the correct file size", async () => {
+  const blob = new Blob([new Uint8Array(fs.readFileSync("test/testfile"))])
+
+  const result = await getFileInformation([new File([blob], "")])
+  expect(result[0].size).toEqual(19)
+})
+
+test("returns the correct last modified date", async () => {
+  const blob = new Blob([new Uint8Array(fs.readFileSync("test/testfile"))])
+
+  const result = await getFileInformation([
+    new File([blob], "", { lastModified: 1584027654000 })
+  ])
+  expect(result[0].lastModified).toEqual(new Date("2020-03-12T15:40:54.000Z"))
+})
+
+test("returns the correct path", async () => {
+  const blob = new Blob([new Uint8Array(fs.readFileSync("test/testfile"))])
+  const file = new File([blob], "")
+  Object.defineProperty(file, "webkitRelativePath", { value: "/a/path" })
+  const result = await getFileInformation([file])
+  expect(result[0].path).toEqual("/a/path")
 })
