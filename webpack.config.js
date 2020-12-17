5a176914da7a3312670b4b076a618e70e3ef548e
TomJKing <tom.king@nationalarchives.gov.uk>
2020-12-17 12:27:08 +0000

    Add polyfills for modules
    Webpack v5 no longer includes node.js core modules polyfills by default

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,45 @@
-const path = require('path')
-const fs = require('fs')
+const path = require("path")
+const fs = require("fs")
 
 module.exports = {
-    entry: './src/index.ts',
-    devtool: 'inline-source-map',
-    module: {
-        rules: [
-            {
-                test: /\.ts?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-        ],
+  entry: "./src/index.ts",
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+    fallback: {
+      crypto: require.resolve("crypto-browserify"),
+      buffer: false,
+      stream: false,
     },
-    resolve: {
-        extensions: ['.ts', '.js'],
-    },
-    plugins: [new DtsBundlePlugin()],
-    output: {
-        filename: 'index.js',
-        libraryTarget: "umd",
-        path: path.resolve(__dirname, 'dist'),
-    },
+  },
+  plugins: [],
+  output: {
+    filename: "index.js",
+    libraryTarget: "umd",
+    path: path.resolve(__dirname, "dist"),
+  },
 }
 
 function DtsBundlePlugin() {}
-DtsBundlePlugin.prototype.apply = function(compiler) {
-    compiler.plugin('done', function() {
-        var dts = require('dts-bundle')
+DtsBundlePlugin.prototype.apply = function (compiler) {
+  compiler.plugin("done", function () {
+    var dts = require("dts-bundle")
 
-        dts.bundle({
-            name: 'tdr',
-            main: 'src/index.d.ts',
-            out: '../dist/index.d.ts',
-            removeSource: true,
-            outputAsModuleFolder: true, // to use npm in-package typings
-        })
+    dts.bundle({
+      name: "tdr",
+      main: "src/index.d.ts",
+      out: "../dist/index.d.ts",
+      removeSource: true,
+      outputAsModuleFolder: true, // to use npm in-package typings
     })
+  })
 }
