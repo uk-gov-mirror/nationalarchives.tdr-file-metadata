10503118a58fb051403cdbc62add9a266b65d666
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2022-05-09 14:13:12 +0100

    Remove the DTS bundler plugin.
    Not strictly related but it's ancient, unmaintained and is causing a
    critical vulnerability.

--- .github/scripts/deploy_npm.sh
+++ .github/scripts/deploy_npm.sh
@@ -6,7 +6,6 @@ git push -u origin $BRANCH_NAME
 npm config set //registry.npmjs.org/:_authToken=$1
 cd ts
 npm ci
-npm run codegen
 npm run build
 npm version patch
 git add package.json package-lock.json
