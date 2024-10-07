96c19e83c00907e0689b6df638ec3f0c007ae0c9
Tom Hallett <tehallett+github@gmail.com>
2024-10-07 09:05:11 +0100

    Updating bot email address

--- .github/scripts/deploy_npm.sh
+++ .github/scripts/deploy_npm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git config --global user.email s-GitHubDABot@nationalarchives.gov.uk
+git config --global user.email 181243999+tna-da-bot@users.noreply.github.com
 git config --global user.name tna-da-bot
 git checkout -b $BRANCH_NAME
 git push -u origin $BRANCH_NAME
