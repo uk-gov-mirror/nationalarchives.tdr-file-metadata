98f1b4d57eb3a7497b8aed0eb01cd94d361be27d
TomJKing <tom.king@nationalarchives.gov.uk>
2026-08-17 14:19:07 +0100

    Fix build

--- .github/scripts/deploy_npm.sh
+++ .github/scripts/deploy_npm.sh
@@ -11,6 +11,6 @@ npm version patch
 git add package.json package-lock.json
 git commit -m 'Update npm version'
 git push
-echo set-npm-version=$(awk '/version/{gsub(/("|",)/,"",$2);print $2}' package.json) >> $GITHUB_OUTPUT
 npm publish --access public
+echo set-npm-version=$(awk '/version/{gsub(/("|",)/,"",$2);print $2}' package.json) >> $GITHUB_OUTPUT
 cd ..
