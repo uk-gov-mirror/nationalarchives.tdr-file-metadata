6c19ac9c90c5a70809a94cab7469c0e926cb8746
Enyo Humphrey-Ackumey <93252496+EnyoHA@users.noreply.github.com>
2026-08-21 10:00:02 +0100

    removed redundant add and commit from script (#1695)

--- .github/scripts/deploy_npm.sh
+++ .github/scripts/deploy_npm.sh
@@ -7,9 +7,7 @@ git push -u origin $BRANCH_NAME
 npm config set //registry.npmjs.org/:_authToken=$1
 npm ci
 npm run build:prod
-npm version patch
-git add package.json package-lock.json
-git commit -m 'Update npm version'
+npm version patch -m 'Update npm version v%s'
 git push
 echo set-npm-version=$(awk '/version/{gsub(/("|",)/,"",$2);print $2}' package.json) >> $GITHUB_OUTPUT
 npm publish --access public
